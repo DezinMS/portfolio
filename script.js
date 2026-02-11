e8ecc562467edad577c2aad91d4b996bab40a559
André Mattos <108373894+DezinMS@users.noreply.github.com>
2026-02-11 02:11:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Pequena animação ao clicar nos projetos
-const projetos = document.querySelectorAll('.project-card a');
-
-projetos.forEach(link => {
-    link.addEventListener('click', () => {
-        alert("Você será redirecionado para o projeto!");
-    });
-});
